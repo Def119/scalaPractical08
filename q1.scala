8222d22ef67464248174e153c71d63bdfe1d4d75
Def119 <75200612+Def119@users.noreply.github.com>
2024-08-12 23:07:31 +0530

    Update q1.scala
    decrypt rounding logic

--- q1.scala
+++ q1.scala
@@ -17,8 +17,9 @@ object q1 extends App {
 
     for (letter <- str) {
       val base = if (letter.isUpper) 'A' else 'a'
-      val newLetter = (letter.toInt - base.toInt - shift) % 26
-      cipher += (newLetter + base.toInt).toChar
+      val newLetter = (letter.toInt - base.toInt - shift + 26) % 26 //add 26 again to circularly reach the letter from the end of the alphabet
+      cipher += (newLetter + base.toInt).toChar                     //ex: A shift left by 2 => -2 in alphabet, but adding base ascii = 65-2=63 is out of the ascii alphabet range....to get the -2 from the z side of alphabet
+                                                                        // add another 26. -2 + 26 = 24 ..... 24 + ascii(a or A) give the letter -2 from z in the ascii table
     }
 
     cipher
